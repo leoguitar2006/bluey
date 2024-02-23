f749fc16c012ab6a620b30dc125cb764bdcad6c1
Marcos Leonardo Martins <mleonardomartins@gmail.com>
2024-02-23 19:48:43 -0300

    feat: add go.sum

--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import "github.com/gin-gonic/gin" // Sempre que não der um nome no import, pega o último nome da path
 
+func main() {
+	r := gin.Default() //:=
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080
 }
